9c126012a1a9d2803d15b1d1bc64fdd861d60929
FlowTraker <flowtrakersealab@gmail.com>
2025-09-28 13:08:35 -0500

    produuction

--- main.py
+++ main.py
@@ -15,9 +15,14 @@ app = FastAPI()
 
 activo = True
 
+origins = [
+    "https://glynne-sst-ai-hsiy.vercel.app",  # tu frontend en Vercel
+    "http://localhost:3000",                  # opcional para pruebas locales
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
